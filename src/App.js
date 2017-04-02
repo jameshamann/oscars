371a5a577113d552fb8812a2ddf8c673fbeb75be
jameshamann <jameshamann@me.com>
2017-04-02 17:15:14 +0100

    adds oscar winners json and years files for select menu

--- src/App.js
+++ src/App.js
@@ -2,17 +2,29 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+var Select = require('react-select');
+
 class App extends Component {
+
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>What won Best Picture the year you were born?</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Choose a year and hit submit to see what won best picture!
         </p>
+        <Select
+          name="form-field-name"
+          options={options}
+          onChange={logChange}
+        />
+      <select id="year" name="year"></select>
+      <input type="submit" value="Submit!"/>
       </div>
     );
   }
